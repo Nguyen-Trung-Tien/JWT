0612a6c902f588cc58d60b7e52b84d8fb7413377
Nguyen-Trung-Tien <2251120447@ut.edu.vn>
2025-05-30 15:44:48 +0700

    handle verify JWT Token

---
+++ Backend/src/middleware/auth.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const white_lists = ["/", "/register", "/login"];
+  if (white_lists.find((item) => "/v1/api" + item === req.originalUrl)) {
+    next();
+  } else {
+    if (req.headers && req.headers.authorization) {
+      const token = req.headers.authorization.split(" ")[1];
+      // verify token
+
+      try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        console.log("Check_TOKEN>>>>", decoded);
+      } catch (error) {
+        res.status(401).json({
+          message: "Access Token ko hop le",
+        });
+      }
+      next();
+    } else {
+      // return exception
+      res.status(401).json({
+        message: "Access Token Header",
+      });
+    }
+  }
+};
+
+module.exports = auth;
--- Backend/src/routes/api.js
+++ Backend/src/routes/api.js
@@ -4,11 +4,11 @@ const {
   handleLogin,
   getUser,
 } = require("../controllers/userController");
-const delay = require("../middleware/delay");
+const auth = require("../middleware/auth");
 const routerAPI = express.Router();
 
-routerAPI.all("*", delay);
-routerAPI.get("/", delay, (req, res) => {
+routerAPI.all("*", auth);
+routerAPI.get("/", (req, res) => {
   return res.status(200).json("hello word");
 });
 
